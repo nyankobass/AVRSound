c9d4973037157af4a93f9e43100b2f9de1fc6aa2
nyankobass <nyankobass@gmail.com>
2019-08-07 22:02:47 +0900

    [update] 将来的に出力を Stereo に対応できるよう DACのインターフェースを変更

--- AVRSound/DAC.cpp
+++ AVRSound/DAC.cpp
@@ -15,7 +15,32 @@ void DAC::Initialize()
     SPI.setClockDivider(SPI_CLOCK_DIV4);
 }
 
-void DAC::Output(uint8_t value)
+void DAC::Output(uint8_t value, LR lr)
+{
+    if (lr == LR::L_CH){
+        OutputL(value);
+    }
+    else {
+        OutputR(value);
+    }
+}
+
+void DAC::OutputL(uint8_t value)
+{
+    uint8_t byte_h = 0b00110000 + ((value >> 4) & 0b00001111);
+    uint8_t byte_l = (value << 4) & 0b11110000;
+
+    /* SS (10pin:PB2) を LOW に */
+    PORTB &= 0b11111011;
+    
+    SPI.transfer(byte_h);    //送受信処理
+    SPI.transfer(byte_l);    //送受信処理
+    
+    /* SS (10pin:PB2) を HIGH に */
+    PORTB |= 0b00000100;
+}
+
+void DAC::OutputR(uint8_t value)
 {
     uint8_t byte_h = 0b00110000 + ((value >> 4) & 0b00001111);
     uint8_t byte_l = (value << 4) & 0b11110000;
--- AVRSound/DAC.h
+++ AVRSound/DAC.h
@@ -6,10 +6,19 @@
 namespace AVRSound {
 class DAC {
 public:
+    enum LR{
+        LR_MIN = 0,
+        L_CH = LR_MIN,
+        R_CH,
+        LR_MAX
+    };
 
     void Initialize();
 
-    void Output(uint8_t value);
+    void Output(uint8_t value, LR lr = LR::L_CH);
+
+    void OutputL(uint8_t value);
+    void OutputR(uint8_t value);
 
 };
 }
